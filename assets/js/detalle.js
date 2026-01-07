d80fc012ce0f2e1e1fb9e169dca397848105ed3a
iVerdugoDev <ivan270@gmail.com>
2026-01-07 20:50:07 -0300

    refactor: refactoriza función para filtrar lugar en detalle.html y crea lugares.js

--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -9,263 +9,6 @@ links.forEach(function (link) {
   }
 });
 
-// Array de lugares: Contendrá listado de ciudades (mínimo 5)
-/* 
-id, nombre del lugar, temperatura actual, estadoActual, pronostico semanal (array de objetos)
-*/
-const lugares = [
-  {
-    id: 1,
-    nombre: 'Santiago',
-    tempActual: 30,
-    estadoActual: 'Soleado',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 16,
-        max: 30,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Martes',
-        min: 18,
-        max: 28,
-        estado: 'Nublado',
-      },
-      {
-        dia: 'Miércoles',
-        min: 15,
-        max: 25,
-        estado: 'Lluvioso',
-      },
-      {
-        dia: 'Jueves',
-        min: 17,
-        max: 27,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Viernes',
-        min: 19,
-        max: 29,
-        estado: 'Parcialmente nublado',
-      },
-      {
-        dia: 'Sábado',
-        min: 20,
-        max: 31,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Domingo',
-        min: 18,
-        max: 26,
-        estado: 'Lluvioso',
-      },
-    ],
-  },
-  {
-    id: 2,
-    nombre: 'Valparaíso',
-    tempActual: 22,
-    estadoActual: 'Nublado',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 14,
-        max: 22,
-        estado: 'Nublado',
-      },
-      {
-        dia: 'Martes',
-        min: 15,
-        max: 24,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Miércoles',
-        min: 13,
-        max: 20,
-        estado: 'Lluvioso',
-      },
-      {
-        dia: 'Jueves',
-        min: 16,
-        max: 23,
-        estado: 'Parcialmente nublado',
-      },
-      {
-        dia: 'Viernes',
-        min: 14,
-        max: 21,
-        estado: 'Nublado',
-      },
-      {
-        dia: 'Sábado',
-        min: 17,
-        max: 25,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Domingo',
-        min: 15,
-        max: 22,
-        estado: 'Lluvioso',
-      },
-    ],
-  },
-  {
-    id: 3,
-    nombre: 'Concepción',
-    tempActual: 18,
-    estadoActual: 'Lluvioso',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 12,
-        max: 18,
-        estado: 'Lluvioso',
-      },
-      {
-        dia: 'Martes',
-        min: 13,
-        max: 19,
-        estado: 'Nublado',
-      },
-      {
-        dia: 'Miércoles',
-        min: 11,
-        max: 17,
-        estado: 'Lluvioso',
-      },
-      {
-        dia: 'Jueves',
-        min: 14,
-        max: 20,
-        estado: 'Parcialmente nublado',
-      },
-      {
-        dia: 'Viernes',
-        min: 12,
-        max: 18,
-        estado: 'Lluvioso',
-      },
-      {
-        dia: 'Sábado',
-        min: 15,
-        max: 21,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Domingo',
-        min: 13,
-        max: 19,
-        estado: 'Nublado',
-      },
-    ],
-  },
-  {
-    id: 4,
-    nombre: 'La Serena',
-    tempActual: 25,
-    estadoActual: 'Parcialmente nublado',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 15,
-        max: 25,
-        estado: 'Parcialmente nublado',
-      },
-      {
-        dia: 'Martes',
-        min: 16,
-        max: 27,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Miércoles',
-        min: 14,
-        max: 24,
-        estado: 'Nublado',
-      },
-      {
-        dia: 'Jueves',
-        min: 17,
-        max: 26,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Viernes',
-        min: 15,
-        max: 23,
-        estado: 'Parcialmente nublado',
-      },
-      {
-        dia: 'Sábado',
-        min: 16,
-        max: 27,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Domingo',
-        min: 14,
-        max: 24,
-        estado: 'Nublado',
-      },
-    ],
-  },
-  {
-    id: 5,
-    nombre: 'Puerto Montt',
-    tempActual: 16,
-    estadoActual: 'Lluvioso',
-    pronosticoSemanal: [
-      {
-        dia: 'Lunes',
-        min: 10,
-        max: 16,
-        estado: 'Lluvioso',
-      },
-      {
-        dia: 'Martes',
-        min: 11,
-        max: 17,
-        estado: 'Nublado',
-      },
-      {
-        dia: 'Miércoles',
-        min: 9,
-        max: 15,
-        estado: 'Lluvioso',
-      },
-      {
-        dia: 'Jueves',
-        min: 12,
-        max: 18,
-        estado: 'Parcialmente nublado',
-      },
-      {
-        dia: 'Viernes',
-        min: 10,
-        max: 16,
-        estado: 'Lluvioso',
-      },
-      {
-        dia: 'Sábado',
-        min: 13,
-        max: 19,
-        estado: 'Soleado',
-      },
-      {
-        dia: 'Domingo',
-        min: 11,
-        max: 17,
-        estado: 'Nublado',
-      },
-    ],
-  },
-];
-
 // Para agregar ícono dinámicamente dependiendo del estado actual del clima
 const ICONOS = {
   Soleado: 'bi-brightness-high',
@@ -282,11 +25,16 @@ const locationId = urlParams.get('id');
 // console.log(locationId);
 
 // 2. Filtrar lugar del array a partir del ID
-const lugarEncontrado = lugares.find((lugar) => {
-  console.log(`Buscando en array lugares el lugar con id: ${locationId}`);
-  return lugar.id == locationId;
-});
-console.log(lugarEncontrado);
+const lugarEncontrado = () => {
+  const lugar = lugares.find((lugar) => {
+    console.log(`Buscando en array lugares el lugar con id: ${locationId}`);
+    return lugar.id == locationId;
+  });
+  return lugar;
+};
+
+// Ejecutar función para buscar lugar a través de su ID
+const ciudadActual = lugarEncontrado();
 
 // 3. Capturar contenedor de información del lugar
 const lugarContainer = document.getElementById('lugar');
@@ -299,18 +47,18 @@ const mostrarLugar = () => {
                 class="col-lg-4 d-flex justify-content-center align-items-center"
               >
                 <i class="bi ${
-                  ICONOS[lugarEncontrado.estadoActual]
+                  ICONOS[ciudadActual.estadoActual]
                 }" style="font-size: 90px"></i>
               </div>
               <div class="col-lg-8">
                 <div class="card-body">
-                  <h2 class="card-title">${lugarEncontrado.nombre}</h2>
+                  <h2 class="card-title">${ciudadActual.nombre}</h2>
                   <ul class="list-group list-group-flush">
                     <li class="list-group-item">${
-                      lugarEncontrado.estadoActual
+                      ciudadActual.estadoActual
                     }</li>
                     <li class="list-group-item">Temperatura: ${
-                      lugarEncontrado.tempActual
+                      ciudadActual.tempActual
                     }°C</li>
                   </ul>
                   <p class="card-text">
