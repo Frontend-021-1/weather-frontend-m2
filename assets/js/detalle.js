4b6697d4d26078518c20a7aadcdb2ef88f231f37
iVerdugoDev <ivan270@gmail.com>
2026-01-23 21:56:39 -0300

    feat: implement WeatherApp class for weather data fetching and rendering

--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -1,4 +1,6 @@
-import lugaresForecastJson from '../../lugares-forecast-api.json' with { type: 'json' };
+import WeatherApp from './classes/WeatherApp.js';
+
+const app = new WeatherApp();
 
 // QuerySelectorAll selecciona todos los elementos. En este caso todos los que tienen clase nav-link
 var links = document.querySelectorAll('.nav-link');
@@ -11,65 +13,38 @@ links.forEach(function (link) {
   }
 });
 
-// 1. Obtener ID enviado por parámetros de la URL
+// 1. Obtener NOMBRE enviado por parámetros de la URL
 const urlParams = new URLSearchParams(window.location.search);
 
-// Extraer id de los parametros
+// Extraer NOMBRE de los parametros
 const locationName = urlParams.get('name');
 // console.log(locationName);
-
-// 2. Filtrar lugar del array a partir del ID
-const lugarEncontrado = () => {
-  return lugaresForecastJson.find(
-    (lugar) => lugar.location.name === locationName,
-  );
-};
-
-// Ejecutar función para buscar lugar a través de su ID
-const ciudadActual = lugarEncontrado();
-console.log(ciudadActual);
 
 // 3. Capturar contenedor de información del lugar
 const lugarContainer = document.getElementById('lugar');
 
-const mostrarLugar = () => {
-  const content = `
-          <div class="card mb-3">
-            <div class="row g-0">
-              <div
-                class="col-lg-4 d-flex justify-content-center align-items-center"
-              >
-                <img src="${ciudadActual.current.condition.icon}" class="card-image-top">
-              </div>
-              <div class="col-lg-8">
-                <div class="card-body">
-                  <h2 class="card-title">${ciudadActual.location.name}</h2>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${
-                      ciudadActual.current.condition.text
-                    }</li>
-                    <li class="list-group-item">Temperatura: ${
-                      ciudadActual.current.temp_c
-                    }°C</li>
-                  </ul>
-                  <p class="card-text">
-                    <small class="text-body-secondary"
-                      >Última actualización hace 3 minutos</small
-                    >
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-  `;
-
-  lugarContainer.innerHTML += content;
-};
-
-mostrarLugar();
-
 // Mostrar sección de pronóstico semanal
 const pronosticoContainer = document.getElementById('pronosticoSemanal');
+
+// Mostrar spinners de carga
+pronosticoContainer.innerHTML = '';
+// Un spinner por cada día de la semana
+for (let i = 0; i < 7; i++) {
+  pronosticoContainer.innerHTML += `
+    <li class="list-group-item">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Cargando...</span>
+      </div>
+    </li>
+  `;
+}
+
+await app.renderForecast(locationName, lugarContainer);
+
+// Limpiar spinners
+pronosticoContainer.innerHTML = '';
+const ciudadActual = app.cityForecast;
+
 // console.log(ciudadActual.pronosticoSemanal);
 ciudadActual.forecast.forecastday.forEach((dia) => {
   console.log(dia);
@@ -83,7 +58,6 @@ ciudadActual.forecast.forecastday.forEach((dia) => {
   pronosticoContainer.innerHTML += content;
 });
 
-// TODO: mostrar estadísticas de la semana
 // 5. Obtener estadísticas semanales y resumir datos
 
 // 5.1 Capturar elementos de la tabla (td -> table datacell)
@@ -91,100 +65,9 @@ const minTempContainer = document.getElementById('minTemp');
 const maxTempContainer = document.getElementById('maxTemp');
 const avTempContainer = document.getElementById('avTemp');
 
-// Función para formatear Números decimales, que en vez de puntos tengan comas
-const formatFloatNumber = (num) => {
-  return num.toLocaleString('es-CL');
-};
-
-const definirEstadoPredominante = (conteoEstados) => {
-  let estadoPredominante;
-
-  if (
-    conteoEstados.Soleado > conteoEstados.Nublado &&
-    conteoEstados.Soleado > conteoEstados.Lluvioso &&
-    conteoEstados.Soleado > conteoEstados['Parcialmente nublado']
-  ) {
-    estadoPredominante = 'Soleado';
-  } else if (
-    conteoEstados.Nublado > conteoEstados.Soleado &&
-    conteoEstados.Nublado > conteoEstados.Lluvioso &&
-    conteoEstados.Nublado > conteoEstados['Parcialmente nublado']
-  ) {
-    estadoPredominante = 'Nublado';
-  } else if (
-    conteoEstados.Lluvioso > conteoEstados.Soleado &&
-    conteoEstados.Lluvioso > conteoEstados.Nublado &&
-    conteoEstados.Lluvioso > conteoEstados['Parcialmente nublado']
-  ) {
-    estadoPredominante = 'Lluvioso';
-  } else if (
-    conteoEstados['Parcialmente nublado'] > conteoEstados.Soleado &&
-    conteoEstados['Parcialmente nublado'] > conteoEstados.Nublado &&
-    conteoEstados['Parcialmente nublado'] > conteoEstados.Lluvioso
-  ) {
-    estadoPredominante = 'Parcialmente Nublado';
-  } else {
-    estadoPredominante = 'variado';
-  }
-
-  return estadoPredominante;
-};
-
 // 5.2 Función para calcular estadísticas, devolverá un objeto con los resultados
-const estadisticasPronostico = () => {
-  // 5.2.1 Obtener temperatura mínima semanal
-  const temperaturasMinimas = ciudadActual.forecast.forecastday.map(
-    (dia) => dia.day.mintemp_c,
-  );
-  // console.log(temperaturasMinimas);
-  const minimaSemanal = Math.min(...temperaturasMinimas);
-  console.log(minimaSemanal);
 
-  // 5.2.2 Obtener temperatura máxima semanal
-  const temperaturasMaximas = ciudadActual.forecast.forecastday.map(
-    (dia) => dia.day.maxtemp_c,
-  );
-  const maximaSemanal = Math.max(...temperaturasMaximas);
-
-  // 5.2.3 Calcular promedio de temperaturas semanal
-  const sumaTemperaturasMaximas = temperaturasMaximas.reduce(
-    (acumulador, actual) => acumulador + actual,
-    0,
-  );
-
-  // promedio = sumaElementos / cantidadElementos
-  let promedioSemanal = parseFloat(
-    (sumaTemperaturasMaximas / temperaturasMaximas.length).toFixed(2),
-  );
-
-  // 5.2.4 Calcular conteo de días por estado del clima
-  const estadosSemanal = ciudadActual.forecast.forecastday.map(
-    (dia) => dia.day.condition.text,
-  );
-
-  const estadosUnicos = [...new Set(estadosSemanal)];
-
-  const conteoEstados = {};
-
-  estadosUnicos.forEach((estado) => {
-    conteoEstados[estado] = ciudadActual.forecast.forecastday.filter(
-      (dia) => dia.day.condition.text === estado,
-    ).length;
-  });
-
-  // 5.2.5 Determinar estado predominante (el más frecuente) de la semana
-  const estadoPredominante = definirEstadoPredominante(conteoEstados);
-
-  return {
-    minimaSemanal,
-    maximaSemanal,
-    promedioSemanal: formatFloatNumber(promedioSemanal),
-    conteoEstados,
-    estadoPredominante,
-  };
-};
-
-const estadisticas = estadisticasPronostico();
+const estadisticas = app.estadisticasPronostico();
 
 minTempContainer.textContent = estadisticas.minimaSemanal;
 maxTempContainer.textContent = estadisticas.maximaSemanal;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,21 @@
 // import lugares from './lugares.js';
-import lugaresJson from '../../lugares-api.json' with { type: 'json' };
+import WeatherApp from './classes/WeatherApp.js';
 
 const lugaresContainer = document.getElementById('lugares');
 
-class WeatherApp {
-  weather = [];
-
-  constructor(lugares) {
-    // Representa todos los lugares que queramos mostrar en la app
-    this.lugares = lugares;
-  }
-
-  // Obtener datos del clima actual de las 5 ciudades
-  fetchWeather() {
-    this.weather = lugaresJson;
-  }
-
-  // Para mostrar lugares en el index
-  insertCardToDom(lugar) {
-    console.log(lugar);
-    const tarjeta = `
-        <div class="col">
-          <div class="card h-100 text-center">
-            <img src="${lugar.current.condition.icon}" class="card-image-top" >
-            <div class="card-body">
-              <h5 class="card-title">${lugar.location.name}</h5>
-              <p class="card-text">${lugar.current.temp_c}°C</p>
-              <p class="card-text">${lugar.current.condition.text}</p>
-            </div>
-            <div class="card-footer bg-transparent border-0">
-              <a class="card-link" href="./detalle.html?name=${
-                lugar.location.name
-              }">Ver detalle</a>
-            </div>
-          </div>
-        </div>
-    `;
-
-    lugaresContainer.innerHTML += tarjeta;
-  }
-
-  // Método que ejecuta petición a api y rellena el dom con respuesta de la api
-  renderWeather() {
-    this.fetchWeather();
-    this.weather.forEach((ciudad) => {
-      this.insertCardToDom(ciudad);
-    });
-  }
-}
-
 // Instancia de clase weatherapp
-const app = new WeatherApp([
+const lugares = [
   'Santiago',
   'Arica',
   'Puerto Natales',
   'Concepción',
   'Talcahuano',
   'Temuco',
-]);
+];
+const app = new WeatherApp();
 
 // Ejecuta método principal de la clase
-app.renderWeather();
+app.renderWeather(lugares, lugaresContainer);
 
 // Manejo del DOM con JS
 // QuerySelectorAll selecciona todos los elementos. En este caso todos los que tienen clase nav-link
